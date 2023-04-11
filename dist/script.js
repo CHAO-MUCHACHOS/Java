9ab67843e60c5f29d335db0a3806173c131c851c
CHAO-MUCHACHOS <kirill2006.sm@gmail.com>
2023-04-11 19:04:08 +0300

    45

--- dist/script.js
+++ dist/script.js
@@ -3,3 +3,12 @@
 //     Переменная = "Hypertext transfer protocol"; (присваивает)
 //     break; (конец)
 // let X = +( prompt ("Введите число") as string)
+// do {
+//     statement1;
+//     statement2;
+//     statement3;
+//    } while(condition)
+// alert()
+// confirm()
+// prompt
+// let num= +(prompt('число ') as string)
