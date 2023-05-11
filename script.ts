98fc431ce04a1dab6e18d5172960646d08f6967b
CHAO-MUCHACHOS <kirill2006.sm@gmail.com>
2023-05-11 19:53:48 +0300

    иав

--- script.ts
+++ script.ts
@@ -124,3 +124,12 @@ let age = +( prompt ("Введите возраст") as string)
 if (age >= 0  || age <= 2){
 alert("ребенок")
 }
+else if (age >= 12  || age <= 18){
+    alert("подростoк")
+}
+else if (age >= 18  || age <= 60){
+    alert("взрослый")
+}
+else (age > 60){
+    alert('Дед')
+}
